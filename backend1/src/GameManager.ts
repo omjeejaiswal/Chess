8a5e77958957cb59564556c165d85a43c8591a01
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-09 23:19:17 +0530

    GameManager

---
+++ backend1/src/GameManager.ts
@@ -0,0 +1,29 @@
+
+import { WebSocket } from "ws";
+
+//User, Game
+
+
+export class GameManager {
+    private games: Game[];
+    private pendingUser: WebSocket;
+    private users: WebSocket[];
+
+    constructor() {
+        this.games = [];
+    }
+
+    addUser(socket: WebSocket) {
+        this.users.push(socket);
+    } 
+
+    removeUser(socket: WebSocket){
+        this.users = this.users.filter(user => user !== socket);
+        // Stop the game here because the user left
+    }
+
+    private handleMessage() {
+
+    }
+
+} 
