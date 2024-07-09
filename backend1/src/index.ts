454fd03b40430263a7db0c15cf10695c810d3afd
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-09 22:35:59 +0530

    .

--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -1,4 +1,16 @@
 
 // websocket  in node.js
 
+import WebSocket from 'ws';
 
+const ws = new WebSocket('ws://www.host.com/path');
+
+ws.on('error', console.error);
+
+ws.on('open', function open() {
+  ws.send('something');
+});
+
+ws.on('message', function message(data) {
+  console.log('received: %s', data);
+});
