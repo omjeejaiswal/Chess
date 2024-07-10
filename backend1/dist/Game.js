ad1c043e964a52aa973e28fbf2e09780b51a3473
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-11 00:44:20 +0530

    game.js

---
+++ backend1/dist/Game.js
@@ -0,0 +1,76 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Game = void 0;
+const chess_js_1 = require("chess.js");
+const message_1 = require("./message");
+class Game {
+    constructor(player1, player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new chess_js_1.Chess();
+        this.startTime = new Date();
+        this.player1.send(JSON.stringify({
+            type: message_1.INIT_GAME,
+            payload: {
+                color: "white"
+            }
+        }));
+        this.player2.send(JSON.stringify({
+            type: message_1.INIT_GAME,
+            payload: {
+                color: "black"
+            }
+        }));
+    }
+    makeMove(socket, move) {
+        // validate the type of move using zod
+        if (this.board.moves.length % 2 === 0 && socket !== this.player1) {
+            return;
+        }
+        if (this.board.moves.length % 2 === 0 && socket !== this.player2) {
+            return;
+        }
+        console.log("did not early return");
+        try {
+            this.board.move(move);
+        }
+        catch (e) {
+            console.log(e);
+            return;
+        }
+        console.log("move successed");
+        // check if the game is Over
+        if (this.board.isGameOver()) {
+            // send the game over message to both player
+            this.player1.emit(JSON.stringify({
+                type: message_1.GAME_OVER,
+                payload: {
+                    winner: this.board.turn() === "w" ? "black" : "white"
+                }
+            }));
+            this.player2.emit(JSON.stringify({
+                type: message_1.GAME_OVER,
+                payload: {
+                    winner: this.board.turn() === "w" ? "black" : "white"
+                }
+            }));
+            return;
+        }
+        console.log(this.board.moves.length % 2);
+        if (this.board.moves.length % 2 === 0) {
+            console.log("sent1");
+            this.player2.emit(JSON.stringify({
+                type: message_1.MOVE,
+                payload: move
+            }));
+        }
+        else {
+            console.log("sent2");
+            this.player1.emit(JSON.stringify({
+                type: message_1.MOVE,
+                payload: move
+            }));
+        }
+    }
+}
+exports.Game = Game;
