54247653ead716c4cc7680dfbd80f1b5c80394b7
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-13 00:55:26 +0530

    game

--- backend1/dist/GameManager.js
+++ backend1/dist/GameManager.js
@@ -1,9 +1,11 @@
 "use strict";
+// import { WebSocket } from "ws";
+// import { Game } from "./Game";
+// import { MOVE, INIT_GAME } from "./message";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.GameManager = void 0;
-const Game_1 = require("./Game");
 const message_1 = require("./message");
-//User, Game
+const Game_1 = require("./Game");
 class GameManager {
     constructor() {
         this.games = [];
@@ -16,32 +18,34 @@ class GameManager {
     }
     removeUser(socket) {
         this.users = this.users.filter(user => user !== socket);
-        // Stop the game here because the user left
     }
     addHandler(socket) {
         socket.on("message", (data) => {
             const message = JSON.parse(data.toString());
+            console.log("Received message:", message);
             if (message.type === message_1.INIT_GAME) {
+                console.log("INIT_GAME message received");
                 if (this.pendingUser) {
-                    // strat a game
+                    console.log("Starting a new game");
                     const game = new Game_1.Game(this.pendingUser, socket);
                     this.games.push(game);
                     this.pendingUser = null;
                 }
                 else {
+                    console.log("Setting pending user");
                     this.pendingUser = socket;
                 }
             }
-            console.log("1");
             if (message.type === message_1.MOVE) {
-                console.log("inside move");
+                console.log("MOVE message received");
                 const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
-                console.log("11");
                 if (game) {
-                    console.log("inside make move");
+                    console.log("Game found, making move:", message.move);
                     game.makeMove(socket, message.move);
                 }
-                console.log("2");
+                else {
+                    console.log("No game found for the move");
+                }
             }
         });
     }
