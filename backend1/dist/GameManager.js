58073c787b81301f5997b12a4f64f079264f0e5c
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-14 00:20:17 +0530

    .

--- backend1/dist/GameManager.js
+++ backend1/dist/GameManager.js
@@ -4,8 +4,9 @@
 // import { MOVE, INIT_GAME } from "./message";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.GameManager = void 0;
-const message_1 = require("./message");
 const Game_1 = require("./Game");
+const message_1 = require("./message");
+// user, game
 class GameManager {
     constructor() {
         this.games = [];
@@ -22,29 +23,21 @@ class GameManager {
     addHandler(socket) {
         socket.on("message", (data) => {
             const message = JSON.parse(data.toString());
-            console.log("Received message:", message);
             if (message.type === message_1.INIT_GAME) {
-                console.log("INIT_GAME message received");
                 if (this.pendingUser) {
-                    console.log("Starting a new game");
                     const game = new Game_1.Game(this.pendingUser, socket);
                     this.games.push(game);
                     this.pendingUser = null;
                 }
                 else {
-                    console.log("Setting pending user");
                     this.pendingUser = socket;
                 }
             }
             if (message.type === message_1.MOVE) {
-                console.log("MOVE message received");
                 const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
                 if (game) {
-                    console.log("Game found, making move:", message.move);
+                    console.log("inside makemove");
                     game.makeMove(socket, message.move);
-                }
-                else {
-                    console.log("No game found for the move");
                 }
             }
         });
