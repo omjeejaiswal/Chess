54aea00dd13235e0bd2b6e2bfeed3e7842a9d0e6
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-14 00:20:36 +0530

    .

--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -1,25 +1,27 @@
 
-// websocket  in node.js
+// import { WebSocketServer } from "ws";
+// import { GameManager } from "./GameManager";
+
+// const wss = new WebSocketServer({port: 8080});
+
+// // inistailze the game manager
+// const gameManager = new GameManager();
+
+// wss.on('connection', function connection(ws){
+//   gameManager.addUser(ws);
+//   ws.on("disconnect", () => gameManager.removeUser(ws));
+// });
+
+
 
 import { WebSocketServer } from "ws";
 import { GameManager } from "./GameManager";
 
-const wss = new WebSocketServer({port: 8080})
+const wss = new WebSocketServer({ port: 8080 });
 
-// inistailze the game manager
 const gameManager = new GameManager();
 
 wss.on('connection', function connection(ws){
-  
-  // ws.on('error', console.error);
-  gameManager.addUser(ws)
-
-  // ws.on('message', function message(data) {
-  //   console.log('received: %s', data);
-  // });
-
-  // ws.send('e4 e5')
-  ws.on("disconnect", () => gameManager.removeUser(ws))
+    gameManager.addUser(ws);
+    ws.on('disconnect', () => gameManager.removeUser(ws))
 });
-
-
