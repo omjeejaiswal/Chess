b3a788f4b4a2d85764189850b5944ff536d5d157
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-14 00:20:31 +0530

    .

--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -33,7 +33,7 @@
 //             if(message.type === INIT_GAME) {
 //                 if(this.pendingUser) {
 //                     // strat a game
-//                     const game = new Game(this.pendingUser, socket)
+//                     const game = new Game(this.pendingUser, socket);
 //                     this.games.push(game);
 //                     this.pendingUser = null;
 //                 }
@@ -56,61 +56,12 @@
 
 
 
-// import { WebSocket } from "ws";
-// import { INIT_GAME, MOVE } from "./message";
-// import { Game } from "./Game";
-
-// export class GameManager() {
-//     private games: Game[];
-//     private pendingUser: WebSocket;
-//     private users: WebSocket[];
-
-//     constructor() {
-//         this.games = [];
-//     }
-
-//     addUser(socket: WebSocket) {
-//         this.users.push(socket);
-//         this.addHandler(socket);
-//     }
-
-//     removeUser(socket: WebSocket){
-//         this.users = this.users.filter(user => user !== socket);
-//     }
-
-//     private addHandler(socket: WebSocket) {
-//         socket.on("message", (data) => {
-//             const message = JSON.parse(data.toString())
-
-//             if(message.type === INIT_GAME){
-//                 if(this.pendingUser) {
-//                     // start a game
-//                     const game = new Game(this.pendingUser, socket)
-//                     this.games.push(game);
-//                     this.pendingUser = null
-//                 }
-//                 else{
-//                     this.pendingUser = socket;
-//                 }
-//             }
-
-//             if(message.type === MOVE) {
-//                 const game = this.games.find(game.player1 === socket || game.palyer2 === socket);
-//                 if(game) {
-//                     game.makeMove(socket, message.move)
-//                 }
-//             }
-//         })
-//     }
-// }
-
-
-
-
 
 import { WebSocket } from "ws";
-import { INIT_GAME, MOVE } from "./message";
 import { Game } from "./Game";
+import { INIT_GAME, MOVE } from "./message";
+
+// user, game
 
 export class GameManager {
     private games: Game[];
@@ -135,31 +86,26 @@ export class GameManager {
     private addHandler(socket: WebSocket) {
         socket.on("message", (data) => {
             const message = JSON.parse(data.toString());
-            console.log("Received message:", message);
 
-            if (message.type === INIT_GAME) {
-                console.log("INIT_GAME message received");
-                if (this.pendingUser) {
-                    console.log("Starting a new game");
+                if (message.type === INIT_GAME) {
+                    if (this.pendingUser) {
                     const game = new Game(this.pendingUser, socket);
                     this.games.push(game);
                     this.pendingUser = null;
-                } else {
-                    console.log("Setting pending user");
+                }
+                else{
                     this.pendingUser = socket;
                 }
             }
 
-            if (message.type === MOVE) {
-                console.log("MOVE message received");
+            if(message.type === MOVE) {
                 const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
-                if (game) {
-                    console.log("Game found, making move:", message.move);
+                if(game) {
+                    console.log("inside makemove");
                     game.makeMove(socket, message.move);
-                } else {
-                    console.log("No game found for the move");
                 }
             }
-        });
+        })
     }
 }
+
