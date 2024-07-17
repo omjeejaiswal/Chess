a7cf406dcc560e3a9a7725c17b45bdf6cb058805
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-18 00:50:37 +0530

    useSocket

---
+++ frontend/src/hooks/useSocket.tsx
@@ -0,0 +1,26 @@
+import { useEffect ,useState } from "react"
+
+const WS_URL = "ws://localhost:8000"
+
+
+export const useSocket = () =>{
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+
+    useEffect(() => {
+        const ws = new WebSocket(WS_URL);
+        ws.onopen = () => {
+            setSocket(ws);
+        }
+
+        ws.onclose = () => {
+            // console.log("dissconneted");
+            setSocket(null);
+        }
+
+        return () => {
+            ws.close();
+        }
+    }, [])
+
+    return socket;
+}
