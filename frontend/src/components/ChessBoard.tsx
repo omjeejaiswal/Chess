3b0ab7218e839a7917d21a3288b65dfcc8c3623f
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-18 00:50:31 +0530

    chessBoard

---
+++ frontend/src/components/ChessBoard.tsx
@@ -0,0 +1,34 @@
+import { Color, PieceSymbol, Square } from "chess.js";
+
+
+
+export const ChessBoard = ({board} : {
+    board: ({
+        square: Square;
+        type: PieceSymbol;
+        color: Color
+
+    } | null)[][]
+}) => {
+    return <div className="text-white-200">
+        {board.map((row, i) => {
+            return <div key={i} className="flex">
+                {row.map((square, j) => {
+                    return <div key={j} className={`w-8 h-8 ${square ? 'bg-green-500 ' : 'bg-green-300'}`}>
+                        {square ? square.type: ""}
+                    </div>
+                })}
+
+            </div>
+        })}
+    </div>
+}
+
+
+
+
+
+
+
+
+
