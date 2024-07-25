4fd4914dc060adb38596c6e299999351be72a6f2
omjeejaiswal <sureshjaiswal808@gmail.com>
2024-07-26 01:05:02 +0530

    adding images code

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -50,7 +50,9 @@ export const ChessBoard = ({chess ,board, socket, setBoard} : {
                     }} key={j} className={`w-16 h-16 ${(i+j)%2 ===0 ? 'bg-green-500 ' : 'bg-white'}`}>
                         <div className="w-full justify-center flex h-full">
                             <div className="h-full justify-center flex-col">
-                            {square ? square.type: ""}
+                                {square ? <img className="w-14" src= {`/${square?.color === "b" ? 
+                                square?.type : `${square?.type?.toUpperCase()} copy`}.png`} />: 
+                                null }
                             </div>
                         </div>
                     </div>
